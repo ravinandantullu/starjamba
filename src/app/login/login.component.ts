ee8d3f7603a62d8d693ca2a347e708cb3692d1b3
Tafsirul <tafsirul.amin@live.com>
2017-05-19 23:00:40 -0700

    code changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public title;
 
-  constructor() { }
+  constructor() { 
+    this.title ="this is coming from the login component";
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
